1fe5bc8605873f0ddc00224448182ff2cde74ce2
Alban Peignier <alban@tryphon.eu>
2011-01-17 10:10:15 +0100

    Fix package in apt::preferences puppet-common

--- manifests/classes/puppet-common.pp
+++ manifests/classes/puppet-common.pp
@@ -1,7 +1,7 @@
 class puppet::common {
   include apt::backports
   apt::preferences { puppet-common:
-    package => puppet, 
+    package => puppet-common, 
     pin => "release a=lenny-backports",
     priority => 999,
     require => Apt::Sources_List["lenny-backports"]
