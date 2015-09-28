4f72daa9e6984408a9720d135eb001989e1430c4
Alban Peignier <alban@tryphon.eu>
2015-09-28 11:26:24 +0200

    Add puppet::ruby to ensure that gem1.9.1 is system gem (required by puppet 2.7)

--- manifests/classes/puppet-common.pp
+++ manifests/classes/puppet-common.pp
@@ -1,8 +1,10 @@
 class puppet::common {
   include apt::backports
+  include puppet::ruby
+
   if $debian::lenny {
     apt::preferences { puppet-common:
-      package => puppet-common, 
+      package => puppet-common,
       pin => "release a=lenny-backports",
       priority => 999,
       require => Apt::Sources_List["lenny-backports"],
@@ -12,11 +14,23 @@ class puppet::common {
 
   if $debian::squeeze {
     apt::preferences { puppet-common:
-      package => puppet-common, 
+      package => puppet-common,
       pin => "release a=squeeze-backports",
       priority => 999,
       require => Apt::Sources_List["squeeze-backports"],
       before => Package[puppet]
     }
   }
+}
+
+class puppet::ruby {
+  # Requires gem command to be ~> 1.9.3
+
+  file { '/usr/bin/gem':
+    ensure => link,
+    target => '/usr/bin/gem1.9.1'
+  }
+
+  # Dummy package (from bearstech ?) via forced links for ruby, gem, etc
+  package { 'ruby': ensure => purged }
 }
