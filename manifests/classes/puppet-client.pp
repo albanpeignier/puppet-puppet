a33a86ef9004048d07fc17461dc7c14258461360
Florent Peyraud <florent@tryphon.eu>
2016-11-04 15:20:24 +0100

    set mode for /var/lib/puppet so that nagios can cross it

--- manifests/classes/puppet-client.pp
+++ manifests/classes/puppet-client.pp
@@ -1,154 +1,175 @@
+# Installs and parameters puppet client
 class puppet::client {
   include debian
   include puppet::common
 
-  package {"facter":
-    ensure  => $facter_version ? {
-      ""      => latest,
-      default => $facter_version,
-    },
-    require => Package["lsb-release"],
-    tag     => "install-puppet",
+  $user_facter_version = $facter_version ? {
+    ''      => latest,
+    default => $facter_version,
   }
 
-  package { puppet:
-    ensure  => $puppet_client_version ? {
-      ""      => latest,
-      default => $puppet_client_version,
-    },
-    require => Package["facter"],
-    tag     => "install-puppet",
+  package {'facter':
+    ensure  => $use_facter_version,
+    require => Package['lsb-release'],
+    tag     => 'install-puppet',
+  }
+
+  $use_puppet_client_version = $puppet_client_version ? {
+    ''      => latest,
+    default => $puppet_client_version,
+  }
+
+  package { 'puppet':
+    ensure  => $use_puppet_client_version,
+    require => Package['facter'],
+    tag     => 'install-puppet',
   }
 
   if $debian::lenny {
-    apt::preferences { puppet:
-      package => puppet, 
-      pin => "release a=lenny-backports",
+    apt::preferences { 'puppet':
+      package  => puppet,
+      pin      => 'release a=lenny-backports',
       priority => 999,
-      before => Package[puppet],
-      require => [Apt::Sources_List["lenny-backports"], Apt::Preferences["puppet-common"]]
+      before   => Package[puppet],
+      require  => [Apt::Sources_List['lenny-backports'], Apt::Preferences['puppet-common']]
     }
   }
 
   if $debian::squeeze {
-    apt::preferences { puppet:
-      package => puppet, 
-      pin => "release a=squeeze-backports",
+    apt::preferences { 'puppet':
+      package  => puppet,
+      pin      => 'release a=squeeze-backports',
       priority => 999,
-      before => Package[puppet],
-      require => [Apt::Sources_List["squeeze-backports"], Apt::Preferences["puppet-common"]]
+      before   => Package[puppet],
+      require  => [Apt::Sources_List['squeeze-backports'], Apt::Preferences['puppet-common']]
     }
   }
 
-  package {"lsb-release":
-    name => $operatingsystem ? {
-      Debian => "lsb-release",
-      Ubuntu => "lsb-release",
-      Redhat => "redhat-lsb",
-      fedora => "redhat-lsb",
-    },
-    ensure => present,
+  $lsb_release_pkg = $::operatingsystem ? {
+    Debian => 'lsb-release',
+    Ubuntu => 'lsb-release',
+    Redhat => 'redhat-lsb',
+    fedora => 'redhat-lsb',
   }
 
-  service { "puppet":
+  package {'lsb-release':
+    ensure => present,
+    name   => $lsb_release_pkg,
+  }
+
+  service { 'puppet':
     ensure    => stopped,
     enable    => false,
     hasstatus => false,
-    tag       => "install-puppet",
-    pattern   => "ruby /usr/sbin/puppetd -w",
+    tag       => 'install-puppet',
+    pattern   => 'ruby /usr/sbin/puppetd -w',
     # make sure the puppet cron is installed before the service is stopped
-    require => [ Cron[puppetd], File["/etc/puppet/puppet.conf"] ]
+    require   => [ Cron[puppetd], File['/etc/puppet/puppet.conf'] ]
   }
 
-  user { "puppet":
-    ensure => present,
-    require => Package["puppet"],
+  user { 'puppet':
+    ensure  => present,
+    require => Package['puppet'],
   }
 
-  file {"/etc/puppet/puppetd.conf": ensure => absent }
+  file {'/etc/puppet/puppetd.conf': ensure => absent }
 
-  file { "/etc/puppet/puppet.conf":
-    source => [ "puppet://$server/files/puppet/client/${fqdn}/puppet.conf",
-      "puppet://$server/files/puppet/client/puppet.conf",
-      "puppet://$server/modules/puppet/client/puppet.conf" ],
-    owner => root, group => 0, mode => 644;
+  file { '/etc/puppet/puppet.conf':
+    source => [ "puppet://${server}/files/puppet/client/${fqdn}/puppet.conf",
+      "puppet://${server}/files/puppet/client/puppet.conf",
+      "puppet://${server}/modules/puppet/client/puppet.conf" ],
+    owner  => 'root',
+    group  => 0,
+    mode   => '0644';
   }
 
-  file { "/etc/puppet/namespaceauth.conf":
-    source => [ "puppet://$server/files/puppet/client/${fqdn}/namespaceauth.conf",
-                "puppet://$server/files/puppet/client/namespaceauth.conf.$operatingsystem",
-                "puppet://$server/files/puppet/client/namespaceauth.conf",
-                "puppet://$server/modules/puppet/client/namespaceauth.conf.$operatingsystem",
-                "puppet://$server/modules/puppet/client/namespaceauth.conf" ],
-    owner => root, group => 0, mode => 600;
+  file { '/etc/puppet/namespaceauth.conf':
+    source => [
+      "puppet://${server}/files/puppet/client/${fqdn}/namespaceauth.conf",
+      "puppet://${server}/files/puppet/client/namespaceauth.conf.${operatingsystem}",
+      "puppet://${server}/files/puppet/client/namespaceauth.conf",
+      "puppet://${server}/modules/puppet/client/namespaceauth.conf.${operatingsystem}",
+      "puppet://${server}/modules/puppet/client/namespaceauth.conf" ],
+    owner  => root,
+    group  => 0,
+    mode   => '0600';
   }
 
-  file {"/var/run/puppet/":
+  file {'/var/run/puppet/':
     ensure => directory,
-    owner  => "puppet",
-    group  => "puppet",
+    owner  => 'puppet',
+    group  => 'puppet',
+  }
+
+  file {'/var/lib/puppet/':
+    ensure => directory,
+    owner  => 'puppet',
+    group  => 'puppet',
+    mode   => '0751',
   }
 
   # Don't start puppet with network interface
-  file { ["/etc/network/if-up.d/puppetd", "/etc/network/if-down.d/puppetd"]:
+  file { ['/etc/network/if-up.d/puppetd', '/etc/network/if-down.d/puppetd']:
     ensure => absent
   }
 
-  $puppet_server = "puppet"
+  $puppet_server = 'puppet'
 
-  file{"/usr/local/sbin/launch-puppet":
-    ensure => present,
-    mode => 755,
-    content => template("puppet/launch-puppet.erb"),
-    tag     => "install-puppet",
+  file{'/usr/local/sbin/launch-puppet':
+    ensure  => present,
+    mode    => '0755',
+    content => template('puppet/launch-puppet.erb'),
+    tag     => 'install-puppet',
   }
 
   # Run puppetd with cron instead of having it hanging around and eating so
   # much memory.
-  cron { "puppetd":
-    ensure  => present,
-    command => "/usr/local/sbin/launch-puppet",
-    user    => 'root',
-    environment => "MAILTO=root",
-    minute  => 15,
-    require => File["/usr/local/sbin/launch-puppet"],
-    tag     => "install-puppet",
-  }         
+  cron { 'puppetd':
+    ensure      => present,
+    command     => '/usr/local/sbin/launch-puppet',
+    user        => 'root',
+    environment => 'MAILTO=root',
+    minute      => 15,
+    require     => File['/usr/local/sbin/launch-puppet'],
+    tag         => 'install-puppet',
+  }
 
-  file { "/etc/cron.d/puppetd":
+  file { '/etc/cron.d/puppetd':
     ensure => absent
   }
 
-  file { "/etc/default/puppet":
-    source => "puppet:///modules/puppet/client/puppet.default",
-    require => Package["puppet"]
+  file { '/etc/default/puppet':
+    source  => 'puppet:///modules/puppet/client/puppet.default',
+    require => Package['puppet']
   }
 
-  file { "/etc/init.d/puppet":
-    source => "puppet:///modules/puppet/client/puppet.initd",
-    mode => 755,
-    require => Package["puppet"]
+  file { '/etc/init.d/puppet':
+    source  => 'puppet:///modules/puppet/client/puppet.initd',
+    mode    => '0755',
+    require => Package['puppet']
   }
 
 }
 
+# Installs Augeas
 class puppet::augeas {
   include apt::tryphon
 
-  package { libaugeas-ruby: }
+  package { 'libaugeas-ruby': }
 
-  file { ["/usr/local/share/augeas", "/usr/local/share/augeas/lenses"]:
-    ensure => directory,
+  file { ['/usr/local/share/augeas', '/usr/local/share/augeas/lenses']:
+    ensure  => directory,
     require => Package[libaugeas-ruby]
   }
 
-  file { "/usr/share/augeas/lenses/contrib": # used by CampToCamp modules
-    ensure => "/usr/local/share/augeas/lenses",
-    require => File["/usr/local/share/augeas/lenses"]
+  file { '/usr/share/augeas/lenses/contrib': # used by CampToCamp modules
+    ensure  => link,
+    target  => '/usr/local/share/augeas/lenses',
+    require => File['/usr/local/share/augeas/lenses']
   }
 
   define lens($source) {
-    file { "/usr/local/share/augeas/lenses/$name.aug":
+    file { "/usr/local/share/augeas/lenses/${name}.aug":
       source => $source
     }
   }
@@ -157,30 +178,31 @@ class puppet::augeas {
     include puppet::augeas::lenny
   }
   
+  # The Debian lenny way of doing things
   class lenny {
-    apt::preferences { libaugeas-ruby:
-      package => libaugeas-ruby, 
-      pin => "release a=lenny-backports",
+    apt::preferences { 'libaugeas-ruby':
+      package  => libaugeas-ruby,
+      pin      => 'release a=lenny-backports',
       priority => 999,
-      before => Package[libaugeas-ruby],
-      require => Apt::Preferences["libaugeas-ruby18"]
+      before   => Package[libaugeas-ruby],
+      require  => Apt::Preferences['libaugeas-ruby18']
     }
 
-    apt::preferences { "libaugeas-ruby18":
-      package => "libaugeas-ruby1.8", 
-      pin => "release a=lenny-backports",
+    apt::preferences { 'libaugeas-ruby18':
+      package  => 'libaugeas-ruby1.8',
+      pin      => 'release a=lenny-backports',
       priority => 999
     }
 
-    apt::preferences { "libaugeas0":
-      package => "libaugeas0", 
-      pin => "release a=lenny-backports",
+    apt::preferences { 'libaugeas0':
+      package  => 'libaugeas0',
+      pin      => 'release a=lenny-backports',
       priority => 999
     }
 
-    apt::preferences { "augeas-lenses":
-      package => "augeas-lenses", 
-      pin => "release a=lenny-backports",
+    apt::preferences { 'augeas-lenses':
+      package  => 'augeas-lenses',
+      pin      => 'release a=lenny-backports',
       priority => 999
     }
   }
