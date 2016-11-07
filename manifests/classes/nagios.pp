ee753d4b6df181c26f6a603c3e19799b2622d6b6
Florent Peyraud <florent@tryphon.eu>
2016-11-07 10:27:55 +0100

    Move service monitoring to puppet module

--- manifests/classes/nagios.pp
+++ manifests/classes/nagios.pp
@@ -1,9 +1,4 @@
 class puppet::nagios {
-  file { '/etc/nagios3/services/puppet.cfg':
-    source => 'puppet:///modules/puppet/nagios/puppet.cfg',
-    require => Package['nagios']
-  }
-
   sudo::conf { 'check_puppetmaster':
     content => "nagios  ALL=(puppet) NOPASSWD: /usr/local/lib/nagios/plugins/check_puppetmaster"
   }
